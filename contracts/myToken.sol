8887e819fb2130527b232e811c3311d5a2426eae
Sachu Shaji Abraham <sachushajiabraham@gmail.com>
2018-11-20 15:55:47 +0530

    initial token setup complted

--- contracts/myToken.sol
+++ contracts/myToken.sol
@@ -7,4 +7,11 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract myToken is ERC20Detailed , ERC20Mintable , ERC20Burnable  , Ownable {
 
+    string private _name = "myToken";
+    string private _symbol= "MIT";
+    uint8 private _decimals= 5;
+    constructor () ERC20Detailed(_name ,_symbol ,_decimals){
+
+    }
+
 }
