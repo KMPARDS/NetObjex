3a7cb3e5cc9f7a7995ce7145140fa115677051da
Sachu Shaji Abraham <sachushajiabraham@gmail.com>
2018-12-12 16:28:04 +0530

    Adding initial structure

--- Projects/Eraswap/contracts/EraswapToken.sol
+++ Projects/Eraswap/contracts/EraswapToken.sol
@@ -7,10 +7,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 
 contract EraswapToken is ERC20Detailed , ERC20Mintable , ERC20Burnable  , Ownable {
 
-    string private _name = "EraswapToken";
-    string private _symbol= "EST";
-    uint8 private _decimals= 18;
-    constructor () public ERC20Detailed(_name ,_symbol ,_decimals){
+    constructor (string  _name, string  _symbol, uint8  _decimals) public ERC20Detailed(_name ,_symbol ,_decimals){
         
     }
 
--- Projects/Eraswap/migrations/2_initial_migration.js
+++ Projects/Eraswap/migrations/2_initial_migration.js
@@ -1,6 +1,9 @@
 const EraswapToken = artifacts.require('./EraswapToken.sol');
 
+const name = 'EraswapToken';
+const symbol = 'EST';
+const decimals = web3.toBigNumber(5);
 
-module.exports = function(deployer) {
-  deployer.deploy(EraswapToken);
+module.exports = function (deployer) {
+  deployer.deploy(EraswapToken, name, symbol, decimals);
 };
