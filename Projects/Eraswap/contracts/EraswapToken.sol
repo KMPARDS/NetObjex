76a7ab44dba78dcc163f081a989bc2aca9c479de
Sachu Shaji Abraham <sachushajiabraham@gmail.com>
2018-12-14 11:26:17 +0530

    Fixed up eraswap token

--- Projects/Eraswap/contracts/EraswapToken.sol
+++ Projects/Eraswap/contracts/EraswapToken.sol
@@ -7,11 +7,15 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol";
 import "openzeppelin-solidity/contracts/token/ERC20/ERC20Pausable.sol";
 
 contract EraswapToken is ERC20Detailed , ERC20Mintable , ERC20Burnable  , Ownable ,ERC20Pausable {
+    string private _name;
+    string private _symbol;
+    uint8 private _decimals;
 
     constructor (string  name, string  symbol, uint8  decimals,uint8 totalsupply) public ERC20Detailed(_name ,_symbol ,_decimals){
         _name = name;
         _symbol = symbol;
         _decimals = decimals;
+        _mint(msg.sender, totalsupply);
     }
 
 }
