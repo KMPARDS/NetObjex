6a90073014b71be1d3e7b6a90d827ebca06d5d9f
Sachu Shaji Abraham <sachushajiabraham@gmail.com>
2018-12-15 12:41:38 +0530

    fixing bug

--- Projects/Eraswap/migrations/2_initial_migration.js
+++ Projects/Eraswap/migrations/2_initial_migration.js
@@ -1,10 +1,13 @@
 const EraswapToken = artifacts.require('./EraswapToken.sol');
-const NRT_Manager = artifacts.require('./NRT_Manager.sol');
+const NRT_Manager = artifacts.require('./NRTManager.sol');
 const name = 'EraswapToken';
 const symbol = 'EST';
 const decimals = web3.toBigNumber(8);
+const totalsupply = web3.toBigNumber(91000000000000000);
 
 module.exports = function (deployer) {
-  deployer.deploy(EraswapToken, name, symbol, decimals);
-  deployer.deploy(NRT_Manager);
+  deployer.deploy(EraswapToken, name, symbol, decimals,totalsupply).then((A)=>{
+    deployer.deploy(NRT_Manager , A.address);
+  });
+  
 };
