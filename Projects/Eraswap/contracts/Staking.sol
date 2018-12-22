b68a72f0ad165e72b1a391642e62576dac99b5b2
Sachu Shaji Abraham <sachushajiabraham@gmail.com>
2018-12-22 22:03:54 +0530

    Optimizing contract

--- Projects/Eraswap/contracts/Staking.sol
+++ Projects/Eraswap/contracts/Staking.sol
@@ -1,9 +1,10 @@
 pragma solidity ^0.4.24;
+
 // contract to manage staking of one year and two year stakers
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 import "./IERC20.sol";
 
-contract Staking{
+contract Staking {
     using SafeMath for uint256;
 
 
@@ -23,42 +24,34 @@ contract Staking{
     uint256 public TwoYearStakersBal;
 
     // orderID to uniquely identify the staking order
-    uint256 OneYearOrderId=100000;
-    uint256 TwoYearOrderId=100000;
+    uint256 OrderId=100000;
 
     // OneYearStakers Details
     struct Staker {
+        bool isActive;
         bool isTwoYear;         // to check whether its one or two year
         bool loan;              // to check whether loan is taken
         uint256 loanCount;      // to check limit of loans that can be taken
         uint256 loanStartTime;  // to keep a check in loan period
         uint256 orderID;        // unique orderid to uniquely identify the order
-        uint256 stakedAmount;   // Amount Staked
+        uint256 stakedamount;   // amount Staked
         uint256 stakedTime;     // Time at which the user staked
 
     }
 
-    
-
-    mapping (uint256 => address) public  OneYearOwnership; // orderid ==> address of user
-    mapping (uint256 => address) public TwoYearOwnership; // orderid ==> address of user
-    
-    mapping (uint256 => Staker) public OneYearStakingDetails;   //orderid ==> order details
-    mapping (uint256 => Staker) public TwoYearStakingDetails;   //orderid ==> order details
+    mapping (uint256 => address) public  StakingOwnership; // orderid ==> address of user
+    mapping (uint256 => Staker) public StakingDetails;   //orderid ==> order details
 
 
   /**
    * @dev Throws if not times up to close a contract
    * @param orderID to identify the unique staking contract
-   * @param isTwoYear to identify whther its one / two year contract
    */
-    modifier isWithinPeriod(uint256 orderID,bool isTwoYear) {
-        if(isTwoYear)
-        {
-        require(now <= TwoYearStakingDetails[orderID].stakedTime + 730 days,"Contract can only be ended after 2 years");
-        }
-        else{
-            require(now <= OneYearStakingDetails[orderID].stakedTime + 365 days,"Contract can only be ended after 1 years");
+    modifier isWithinPeriod(uint256 orderID) {
+        if (StakingDetails[orderID].isTwoYear) {
+        require(now <= StakingDetails[orderID].stakedTime + 730 days,"Contract can only be ended after 2 years");
+        }else {
+        require(now <= StakingDetails[orderID].stakedTime + 365 days,"Contract can only be ended after 1 years");
         }
         _;
     }
@@ -66,146 +59,101 @@ contract Staking{
     /**
    * @dev To check if loan is initiated
    * @param orderID to identify the unique staking contract
-   * @param isTwoYear to identify whther its one / two year contract
    */
-   modifier isNoLoanTaken(uint256 orderID,bool isTwoYear) {
-        if(isTwoYear)
-        {
-        require(TwoYearStakingDetails[orderID].loan != true,"Loan is present");
-        }
-        else{
-            require(OneYearStakingDetails[orderID].loan != true,"Loan is present");
-        }
+   modifier isNoLoanTaken(uint256 orderID) {
+        require(StakingDetails[orderID].loan != true,"Loan is present");
         _;
     }
 
         /**
    * @dev To check whether its valid staker 
    * @param orderID to identify the unique staking contract
-   * @param isTwoYear to identify whther its one / two year contract
    */
-   modifier onlyStakeOwner(uint256 orderID,bool isTwoYear) {
-        if(isTwoYear)
-        {
-        require(TwoYearOwnership[orderID] == msg.sender,"Staking owner should be valid");
-        }
-        else{
-        require(OneYearOwnership[orderID] == msg.sender,"Staking owner should be valid");
-        }
+   modifier onlyStakeOwner(uint256 orderID) {
+        require(StakingOwnership[orderID] == msg.sender,"Staking owner should be valid");
         _;
     }
 
    /**
    * @dev To create staking contract
-   * @param Amount Total Est which is to be Staked
-   * @param isTwoYear to identify whther its one / two year contract
+   * @param amount Total Est which is to be Staked
    * @return orderId of created 
    */
 
-    function createStakingContract(uint256 Amount,bool isTwoYear) external returns (uint256){ 
-        if(isTwoYear){
-            TwoYearOrderId = TwoYearOrderId.add(1);
+    function createStakingContract(uint256 amount,bool isTwoYear) external returns (uint256) { 
+            OrderId = OrderId.add(1);
+            StakingOwnership[OrderId] = msg.sender;
+            if (isTwoYear) {
             TwoYearStakerCount = TwoYearStakerCount.add(1);
-            TwoYearStakersBal = TwoYearStakersBal.add(Amount);
-            TwoYearOwnership[TwoYearOrderId] = msg.sender;
-            TwoYearStakingDetails[TwoYearOrderId] = Staker(true,false,0,0,TwoYearOrderId,Amount, 730 days);
-            require(tokenContract.transfer(address(this), Amount), "The token transfer should be done");
-            return TwoYearOrderId;
-        }
-        else{
-            OneYearOrderId = OneYearOrderId.add(1);
+            TwoYearStakersBal = TwoYearStakersBal.add(amount);
+            StakingDetails[OrderId] = Staker(true,true,false,0,0,OrderId,amount, now);
+            }else {
             OneYearStakerCount = OneYearStakerCount.add(1);
-            OneYearStakersBal = OneYearStakersBal.add(Amount);
-            OneYearOwnership[OneYearOrderId] = msg.sender;
-            OneYearStakingDetails[OneYearOrderId] = Staker(false,false,0,0,OneYearOrderId,Amount, 365 days);
-            require(tokenContract.transfer(address(this), Amount), "The token transfer should be done");
-            return OneYearOrderId;
+            OneYearStakersBal = OneYearStakersBal.add(amount);
+            StakingDetails[OrderId] = Staker(true,false,false,0,0,OrderId,amount, now);
+            }
+            require(tokenContract.transfer(address(this), amount), "The token transfer should be done");
+            return OrderId;
         }
-    }
  
     /**
    * @dev To check if loan is initiated
    * @param orderId Total Est which is to be Staked
-   * @param isTwoYear to identify whther its one / two year contract
    * @return orderId of created 
    */
-  function takeLoan(uint256 orderId, bool isTwoYear) onlyStakeOwner(orderId,isTwoYear) isNoLoanTaken(orderId, isTwoYear) isWithinPeriod(orderId,isTwoYear) external returns (bool){
-    if(isTwoYear){
-          require(TwoYearStakingDetails[orderId].loanCount <= 1 ,"only one loan per year is allowed");
-          require((TwoYearStakingDetails[orderId].stakedTime).sub(now)>= 60 days,"Contract End is near");
+  function takeLoan(uint256 orderId) onlyStakeOwner(orderId) isNoLoanTaken(orderId) isWithinPeriod(orderId) external returns (bool) {
+    require((StakingDetails[orderId].stakedTime).sub(now) >= 60 days,"Contract End is near");
+    if (StakingDetails[orderId].isTwoYear) {
+          require(StakingDetails[orderId].loanCount <= 1,"only one loan per year is allowed");        
           TwoYearStakerCount = TwoYearStakerCount.sub(1);
-          TwoYearStakersBal = TwoYearStakersBal.sub(TwoYearStakingDetails[orderId].stakedAmount);
-          TwoYearStakingDetails[orderId].loan =true;
-          TwoYearStakingDetails[orderId].loanStartTime = now;
-          TwoYearStakingDetails[orderId].loanCount = (TwoYearStakingDetails[orderId].loanCount).add(1);
-          require(tokenContract.transfer(msg.sender,(TwoYearStakingDetails[orderId].stakedAmount).div(2)),"The contract should transfer loan amount");
-          return true;
-      }
-      else{
-          require(OneYearStakingDetails[orderId].loanCount == 0,"only one loan per year is allowed");
-          require((OneYearStakingDetails[orderId].stakedTime).sub(now)>= 60 days,"Contract End is near");
+          TwoYearStakersBal = TwoYearStakersBal.sub(StakingDetails[orderId].stakedamount);
+    }else {
+          require(StakingDetails[orderId].loanCount == 0,"only one loan per year is allowed");        
           OneYearStakerCount = OneYearStakerCount.sub(1);
-          OneYearStakersBal = OneYearStakersBal.sub(OneYearStakingDetails[orderId].stakedAmount);
-          OneYearStakingDetails[orderId].loan =true;
-          OneYearStakingDetails[orderId].loanStartTime = now;
-          OneYearStakingDetails[orderId].loanCount = (OneYearStakingDetails[orderId].loanCount).add(1);
-          require(tokenContract.transfer(msg.sender,(OneYearStakingDetails[orderId].stakedAmount).div(2)),"The contract should transfer loan amount");
+          OneYearStakersBal = OneYearStakersBal.sub(StakingDetails[orderId].stakedamount);
+    }
+          StakingDetails[orderId].loan = true;
+          StakingDetails[orderId].loanStartTime = now;
+          StakingDetails[orderId].loanCount = (StakingDetails[orderId].loanCount).add(1);
+          require(tokenContract.transfer(msg.sender,(StakingDetails[orderId].stakedamount).div(2)),"The contract should transfer loan amount");
           return true;
       }
-  }
+      
   /**
    * @dev To repay the leased loan
    * @param orderId Total Est which is to be Staked
-   * @param isTwoYear to identify whther its one / two year contract
    * @return total repayment
    */
 
-  function calculateTotalPayment(uint256 orderId, bool isTwoYear) public view returns (uint256){
-      if(isTwoYear){
-          return ((TwoYearStakingDetails[orderId].stakedAmount).div(200)).mul(101);
-      }
-      else{
-          return ((OneYearStakingDetails[orderId].stakedAmount).div(200)).mul(101);
-      }
+  function calculateTotalPayment(uint256 orderId) public view returns (uint256){
+          return ((StakingDetails[orderId].stakedamount).div(200)).mul(101);
+      
   }
 
-  function isEligibleForRepayment(uint256 orderId, bool isTwoYear) public view returns (bool){
-      if(isTwoYear){
-          require(TwoYearStakingDetails[orderId].loan == true,"User should have taken loan");
-          require((TwoYearStakingDetails[orderId].loanStartTime).sub(now) < 60 days,"Loan repayment should be done on time");
-      }
-      else{
-          require(OneYearStakingDetails[orderId].loan == true,"User should have taken loan");
-          require((OneYearStakingDetails[orderId].loanStartTime).sub(now) < 60 days,"Loan repayment should be done on time");
-      }
-      return true;
+  function isEligibleForRepayment(uint256 orderId) public view returns (bool){
+          require(StakingDetails[orderId].loan == true,"User should have taken loan");
+          require((StakingDetails[orderId].loanStartTime).sub(now) < 60 days,"Loan repayment should be done on time");
+          return true;
   }
    /**
    * @dev To repay the leased loan
    * @param orderId Total Est which is to be Staked
-   * @param isTwoYear to identify whther its one / two year contract
    * @return true if success
    */
-  function rePayLoan(uint256 orderId, bool isTwoYear) onlyStakeOwner(orderId,isTwoYear) isWithinPeriod(orderId,isTwoYear) external returns (bool){
-      require(isEligibleForRepayment( orderId, isTwoYear),"The user should be eligible for repayment");
-      if(isTwoYear){
-          TwoYearStakingDetails[orderId].loan = false;
-          TwoYearStakingDetails[orderId].loanStartTime = 0;
-          luckPoolBal = luckPoolBal.add((TwoYearStakingDetails[orderId].stakedAmount).div(200));
+  function rePayLoan(uint256 orderId) onlyStakeOwner(orderId) isWithinPeriod(orderId) external returns (bool){
+      require(isEligibleForRepayment(orderId),"The user should be eligible for repayment");
+      StakingDetails[orderId].loan = false;
+      StakingDetails[orderId].loanStartTime = 0;
+      luckPoolBal = luckPoolBal.add((StakingDetails[orderId].stakedamount).div(200));
+      if (StakingDetails[orderId].isTwoYear) {  
           TwoYearStakerCount = TwoYearStakerCount.add(1);
-          TwoYearStakersBal = TwoYearStakersBal.add(TwoYearStakingDetails[orderId].stakedAmount);
-          require(tokenContract.transfer(address(this),calculateTotalPayment( orderId, isTwoYear)) ,"The contract should receive loan amount with interest");
-          return true;
-      }
-      else{
-          OneYearStakingDetails[orderId].loan = false;
-          OneYearStakingDetails[orderId].loanStartTime = 0;
-          luckPoolBal = luckPoolBal.add((OneYearStakingDetails[orderId].stakedAmount).div(200));
+          TwoYearStakersBal = TwoYearStakersBal.add(StakingDetails[orderId].stakedamount);
+      }else {  
           OneYearStakerCount = OneYearStakerCount.add(1);
-          OneYearStakersBal = OneYearStakersBal.add(OneYearStakingDetails[orderId].stakedAmount);
-          require(tokenContract.transfer(address(this),calculateTotalPayment( orderId, isTwoYear)),"The contract should receive loan amount with interest");
-          return true;
+          OneYearStakersBal = OneYearStakersBal.add(StakingDetails[orderId].stakedamount);
       }
+          require(tokenContract.transfer(address(this),calculateTotalPayment(orderId)),"The contract should receive loan amount with interest");
+          return true;
   }
   //should burn defaulters token and update balances in stakers
 //   function updateStakers(){
@@ -214,7 +162,7 @@ contract Staking{
    // function releaseMonthlyReturn
 
 
-//   function windUpContract() external{
-
+//   function windUpContract(uint256 orderId) onlyStakeOwner(orderId)  external {
+    
 //   }
 }
