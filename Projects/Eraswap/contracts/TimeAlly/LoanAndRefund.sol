8f8bcc84ed2b9bbd2ff6cbf346805953846541a1
Sreeraj S <ssreerajmailbox@gmail.com>
2019-02-11 14:25:17 +0530

    Add files via upload

---
+++ Projects/Eraswap/contracts/TimeAlly/LoanAndRefund.sol
@@ -0,0 +1,145 @@
+pragma solidity ^0.4.24;
+import "./SafeMath.sol";
+
+contract LoanAndRefund{
+  using SafeMath for uint256;
+
+  struct Loan {
+  uint128 LoanAmount;
+  uint32 LoanPeriod;
+  uint32 loanStartTime;
+  uint32 LoanListIndex;
+  }
+  mapping (uint256 => Loan) public Loans;
+
+  struct Refund {
+  uint32 RefundWeeks;
+  uint32 Refundcount;
+  uint32 RefundListIndex;
+   uint64 RefundAmount;
+  }
+  mapping (uint256 => Refund) public Refunds;
+
+  uint256[] public RefundList;
+  uint256[] public LoanList;
+  address TimeAlly;
+
+event RefundInitiated(uint256 contractid, uint256 count, uint256 amount);
+event RefundEnded(uint256 contractid);
+event LoanDefaulted(uint256 contractid);
+
+
+   modifier OnlyTimeAlly() {
+     require(msg.sender == TimeAlly, "Owner TimeAlly should be calling");
+     _;
+   }
+
+   constructor(address timeally) public {
+     TimeAlly = timeally;
+   }
+
+  function ViewLoan(uint256 contractID) public OnlyTimeAlly() view returns(uint256, uint256, uint256){
+   return(uint256(Loans[contractID].LoanPeriod), uint256(Loans[contractID].loanStartTime), uint256(Loans[contractID].LoanAmount));
+  }
+
+    function ViewRefund(uint256 contractID) public OnlyTimeAlly() view returns(uint256, uint256, uint256){
+   return(uint256(Refunds[contractID].RefundWeeks), uint256(Refunds[contractID].Refundcount), uint256(Refunds[contractID].RefundAmount));
+  }
+
+  function AddLoan(uint256 contractID, uint32 loanperiod, uint128 loanamount) public OnlyTimeAlly() returns(bool) {
+    Loan memory loan;
+    loan.LoanPeriod = loanperiod;
+    loan.LoanAmount = uint128(loanamount);
+    loan.loanStartTime = uint32(now);
+    loan.LoanListIndex = uint32(LoanList.push(contractID).sub(1));
+    Loans[contractID] = loan;
+  return true;
+  }
+
+  function RemoveLoan(uint256 contractID) public OnlyTimeAlly() returns(bool) {
+  DeleteLoanListElement(Loans[contractID].LoanListIndex);
+  return true;
+  }
+
+  function AddRefund(uint256 contractID, uint32 refundWeeks, uint32 refundcount, uint64 refundamount) public OnlyTimeAlly() returns(bool) {
+  Refund memory refund;
+  refund.RefundWeeks = refundWeeks;
+  refund.Refundcount = refundcount;
+  refund.RefundAmount = refundamount;
+  refund.RefundListIndex = uint32(RefundList.push(contractID).sub(1));
+
+  Refunds[contractID] = refund;
+  return true;
+  }
+
+  function MonthlyRefundHandler() public OnlyTimeAlly() returns (uint[]){
+    uint256[] UserPayment;
+    uint256 character;
+    Refund memory refund;
+    bool check = false;
+    for (uint256 i = 0; i < RefundList.length; i++) {
+
+     if(check == true){
+            i--;
+            check = false;
+        }
+      uint256 contractID = RefundList[i];
+      refund = Refunds[contractID];
+      character = contractID;
+      character |= refund.RefundAmount<<128;
+      UserPayment.push(character);
+      emit RefundInitiated(contractID, refund.Refundcount, refund.RefundAmount);
+      refund.Refundcount++;
+      Refunds[contractID] = refund;
+      if(refund.Refundcount == refund.RefundWeeks){
+        DeleteRefundListElement(refund.RefundListIndex);
+        emit RefundEnded(contractID);
+       check = true;
+      }
+    }
+    return(UserPayment);
+  }
+
+  function MonthlyLoanHandler() public OnlyTimeAlly() returns (uint[]){
+    uint256[] Defaultlist;
+    Loan memory loan;
+    bool check = false;
+    for (uint256 i = 0; i < LoanList.length; i++) {
+         if(check == true){
+            i--;
+            check = false;
+        }
+      uint256 contractID = LoanList[i];
+      loan = Loans[contractID];
+      if ((now.sub(loan.loanStartTime)) > loan.LoanPeriod ) {
+        Defaultlist.push(contractID);
+        DeleteLoanListElement(loan.LoanListIndex);
+        emit LoanDefaulted(contractID);
+        check = true;
+      }
+    }
+    return(Defaultlist);
+  }
+
+
+    function DeleteRefundListElement(uint32 index) internal returns(bool){
+      require(index < RefundList.length);
+      uint256 last = RefundList.length-1;
+      Refunds[RefundList[last]].RefundListIndex = index;
+      RefundList[index] = RefundList[last];
+      delete RefundList[last];
+      RefundList.length--;
+      return true;
+    }
+
+    function DeleteLoanListElement(uint32 index) internal returns(bool){
+      require(index < LoanList.length);
+      uint256 last = LoanList.length-1;
+      Loans[LoanList[last]].LoanListIndex = index;
+      LoanList[index] = LoanList[last];
+      delete LoanList[last];
+      LoanList.length--;
+      return true;
+    }
+
+}
---
+++ Projects/Eraswap/contracts/TimeAlly/TimeAllyCore.sol
@@ -0,0 +1,183 @@
+pragma solidity ^0.4.24;
+
+import "./SafeMath.sol";
+import "./Staking.sol";
+import "./LoanAndRefund.sol";
+//import "./EraswapToken.sol";
+
+contract TimeAllyCore {
+
+using SafeMath for uint256;
+
+  struct TimeAllyPlan{                         //Structure to store details of different Plans
+  uint256 PlanPeriod;
+  uint256 LoanInterestRate;
+  uint256 LoanPeriod;
+  uint256 RefundWeeks;
+  }
+  uint256 public PlanID = 0;
+  mapping (uint256 => TimeAllyPlan) public Plans;
+
+  struct Contract {
+  uint256 status;
+  uint256 planid;
+  address owner;
+  uint256 timestamp;
+  }
+  uint256 public ContractID = 0;
+  mapping (uint256 => Contract) public Contracts;
+  mapping (uint256 => uint256) public LoanRepaymentAmount;
+  mapping (address => uint256[]) ContractIds;
+
+  uint256 public LastMonthlyHandler;
+  uint256 public PlanHandlerCount = 0;
+  uint256 public MonthlyHandlerCount = 0;
+  bool Paused = false;
+
+uint256[] public TokenTransferList;
+address Owner;
+address public EraswapTokenAddress;
+//EraswapToken EraswapTokens;
+address public StakingAddress;
+Staking   staking;
+address public LoanandRefundAddress;
+LoanAndRefund   loanAndRefund;
+
+
+event NRTRecieved(uint256 nrt);
+event MonthlyPaymentsHandler(address user, uint256 amount);
+event ContractBurned(uint256 contractid, uint256 amount);
+event PlanHandlercompleted(uint256 id);
+event MonthlyPlanHandlerscompleted();
+event MonthlyPaymentHandlercompleted();
+event TokenSent(address[100], uint256[100]);
+
+modifier OnlyOwner() {
+  require(msg.sender == Owner, "Owner TimeAlly should be calling");
+  _;
+}
+
+modifier NotPaused() {
+  require(Paused == false, "SHould not be Paused");
+  _;
+}
+
+
+constructor() public{
+  LastMonthlyHandler = now;
+  Owner = msg.sender;
+}
+
+function Setaddress(address stakingaddress, address loanandrefundaddress) public OnlyOwner() returns(bool){
+    StakingAddress = stakingaddress;
+    staking = Staking(StakingAddress);
+    LoanandRefundAddress = loanandrefundaddress;
+    loanAndRefund = LoanAndRefund(LoanandRefundAddress);
+    return true;
+}
+
+
+function MonthlyMasterHandler() external OnlyOwner() returns(bool){
+  require(now.sub(LastMonthlyHandler)> 1 minutes);
+  //require(now.sub(EraswapTokens.LastNRTRelease())< 2592000);
+  require(MonthlyHandlerCount<3);
+  Paused = true;
+  if(MonthlyHandlerCount == 0){
+  uint256 NRT = 100000; //EraswapTokens.TimeAllyNRT();
+  uint256 luckPoolBal = staking.MonthlyNRTHandler(NRT, PlanID);
+ // if(luckPoolBal != 0){
+  //  require(EraswapTokens.increaseApproval(EraswapTokenAddress, luckPoolBal));
+   // require(EraswapTokens.UpdateLuckpool(luckPoolBal));
+  //}
+  emit NRTRecieved(NRT);
+  MonthlyHandlerCount = 1;
+}
+
+  else if(MonthlyHandlerCount == 1){
+  uint256[] memory RefundList;
+  RefundList = loanAndRefund.MonthlyRefundHandler();
+  AddToTransferList(RefundList);
+  MonthlyHandlerCount = 2;
+}
+
+  else if(MonthlyHandlerCount == 2){
+  uint256[] memory LoanList;
+  LoanList = loanAndRefund.MonthlyLoanHandler();
+  for (uint256 i = 0; i < LoanList.length; i++) {
+    uint256 contractID = LoanList[i];
+    uint256 amount = staking.ViewStakedAmount(contractID);
+    //require(EraswapTokens.increaseApproval(EraswapTokenAddress, amount));
+    //require(EraswapTokens.UpdateBurnBal(amount));
+    Contracts[contractID].status = 4;
+    emit ContractBurned(contractID, amount);
+  }
+  MonthlyHandlerCount = 3;
+}
+}
+
+function MonthlyPlansHandler(uint256 Size) external OnlyOwner() returns(bool){
+  require(MonthlyHandlerCount == 3);
+  require(PlanHandlerCount <= PlanID);
+  uint256[] memory InterestList;
+  bool status;
+  (InterestList, status) = staking.MonthlyPlanHandler(PlanHandlerCount, Size);
+  AddToTransferList(InterestList);
+  if(status == true){
+  emit PlanHandlercompleted(PlanHandlerCount);
+  PlanHandlerCount++;
+  }
+  if(PlanHandlerCount > PlanID){
+  emit MonthlyPlanHandlerscompleted();
+  MonthlyHandlerCount = 4;
+  PlanHandlerCount = 0;
+  }
+  return status;
+}
+
+
+function MonthlyPaymentHandler() external OnlyOwner() returns(bool){
+require(MonthlyHandlerCount == 4);
+uint256[100] paymentlist;
+address[100] addresslist;
+uint256 contractID;
+uint256 amount;
+address add;
+uint256 start = 0;
+uint256 stop = 100;
+while(start < TokenTransferList.length){
+if((start+100) > TokenTransferList.length)
+{
+   stop = TokenTransferList.length - start;
+   for(i = stop; i<100; i++){
+     addresslist[i] = address(0);
+     paymentlist[i] = 0;
+   }
+}
+  for (uint256 i = 0; i < stop; i++) {
+      contractID = uint256(TokenTransferList[start+i]);
+      amount = uint256(uint128(TokenTransferList[start+i]>>128));
+      add = Contracts[contractID].owner;
+      addresslist[i] = add;
+      paymentlist[i] = amount;
+  }
+  //require(EraswapTokens.UpdateBalance(addresslist, paymentlist));
+  emit TokenSent(addresslist, paymentlist);
+  start = start + 100;
+}
+emit MonthlyPaymentHandlercompleted();
+delete TokenTransferList;
+MonthlyHandlerCount = 0;
+LastMonthlyHandler = now;
+Paused = false;
+}
+
+
+function AddToTransferList(uint256[] list) internal returns(bool) {
+  for (uint256 i = 0; i < list.length; i++) {
+    TokenTransferList.push(list[i]);
+  }
+  return true;
+}
+
+
+}
